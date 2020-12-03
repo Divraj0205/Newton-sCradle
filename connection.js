c06ea0362805d0bb9533bf3698d0809fa220936d
Divraj Walia <70948686+Divraj0205@users.noreply.github.com>
2020-12-03 13:52:25 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,24 @@
+class connection{
+    constructor(bodyA,bodyB,offsetX,offsetY){
+        this.offsetX=offsetX;
+        this.offsetY=offsetY;
+    var options = {
+        bodyA: bodyA,
+        bodyB: bodyB,
+        length: 200,
+        stiffness: 0.04,
+        pointB:{x:this.offsetX,y:this.offsetY}
+    }
+      
+   this.chain=Matter.Constraint.create(options);
+    World.add(world, this.chain);
+}
+
+    display(){
+     var posA=this.chain.bodyA.position;
+     var posB=this.chain.bodyB.position;
+     line(posA.x,posA.y,posB.x,posB.y); 
+
+    }
+
+}
